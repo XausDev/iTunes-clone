5d5d5929bc714bbdca8a5885f460f7e87add8f74
XausDev <eric.xc@hotmail.com>
2024-02-19 16:10:16 +0100

    funcion de ordenar y background-color en los "li" clicados

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
 const ul = document.getElementById('music-list');
 
 function pintarResultados(items) {
-    let str = '';
-    items.forEach((cur, index) => {
+
+    ul.innerHTML = "";//Esto hace que se "recargue" el listado
+
+    ordenarResultados(items);
+
+    console.log(items);
+
+    items.forEach((item, index) => {
         const li = document.createElement('li');
         li.id = 'item-' + index; // Genera un ID único para cada elemento li
-        if (cur.kind == 'song') {
-        li.innerHTML = `<img src="images/musica.png" alt="Icono" width="12" height="12">
-                        <span>${cur.artistName} - ${cur.trackName}</span>`;
+        
+        if (item.kind == 'song') {
+            li.innerHTML = `<img src="images/musica.png" alt="Icono" width="12" height="12">
+                        <span>${item.artistName} - ${item.trackName}</span>`;
         }else {
             li.innerHTML =`<img src="images/camara-de-video.png" alt="Icono" width="12" height="12">
-                           <span>${cur.artistName} - ${cur.trackName}</span>`;
+                           <span>${item.artistName} - ${item.trackName}</span>`;
         }
 
         ul.appendChild(li);
 
-       
-
         li.addEventListener('click', () => {
+            li.style.backgroundColor = '#EDEEEE';
             let controls = document.getElementById('display-controls'); // Accede al elemento li que fue clickeado
-            let previewUrl = cur.previewUrl;
-            if (cur.kind == 'song') {
+            let previewUrl = item.previewUrl;
+            if (item.kind == 'song') {
                 controls.innerHTML = `
                 <div class="col-3">
-                    <img src="${cur.artworkUrl100}" alt="portada del album" width="100" height="100">
+                    <img src="${item.artworkUrl100}" alt="portada del album" width="100" height="100">
                 </div>
                 <div class="col-1"></div>
                 <div class="col-8">
-                    <div class="row">${cur.trackName}</div>
-                    <div class="row">${cur.artistName}</div>
+                    <div class="row">${item.trackName}</div>
+                    <div class="row">${item.artistName}</div>
                 </div>
                 <div class="row-12 controles">
                     <audio class="col-12" src="${previewUrl}" controls><source></audio>
                 </div>`;
-            } else if (cur.kind == 'tv-episode') {
+            } else if (item.kind == 'tv-episode') {
                 controls.innerHTML = `
                 <div id="videoContent" class="col-12">
                     <iframe src="${previewUrl}" width="100%" height="100%" frameborder="0"></iframe>
@@ -41,6 +47,31 @@ function pintarResultados(items) {
             }
         });
     });
+}
+
+function ordenarResultados(items){ //Se asigna un atributo ordenación segun kind y luego se ordena
+
+    items.forEach((item) => {
+        if (item.kind === "song") {
+            item.ordenacion = 1;
+        } else if (item.kind === "tv-episode") {
+            item.ordenacion = 2;
+        } else {
+            item.ordenacion = 3;
+        }
+    });
+
+    items.sort((a,b) =>{
+
+        if (a.ordenacion > b.ordenacion) {
+            return 1;
+        }
+        if (a.ordenacion < b.ordenacion) {
+            return -1;
+        }
+        return 0;
+    });
+
 }
 
 
@@ -56,15 +87,15 @@ fetch(url)
 
 
 function buscarAPI() {
-    var searchText = document.getElementById("searchText").value;
-    var busqueda = searchText.replaceAll(" ", "+");
+    let searchText = document.getElementById("searchText").value;
+    let busqueda = searchText.replaceAll(" ", "+");
 
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open("GET", "https://itunes.apple.com/search?term=" + busqueda);
     xhr.onload = function() {
         if (xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
-            console.log(response);
+            let response = JSON.parse(xhr.responseText);
+            //console.log(response);
             pintarResultados(response.results);
         } else {
             console.log("Error" + xhr.statusText);
@@ -75,21 +106,3 @@ function buscarAPI() {
     };
     xhr.send();
 };
-
-//Boton search
-
-/*document.getElementById('search-btn').addEventListener
-('click', ()=>{
-    const controls = document.getElementById('display-controls');
-    controls.innerHTML = `<div class="col-3">
-        <img src="images/ramones.png" alt="portada del album" width="100" height="100">
-        </div>
-        <div class="col-1"></div>
-        <div class="col-8">
-            <div class="row">titulo</div>
-            <div class="row">artista</div>
-        </div>
-        <div class="row-12 controles">
-            <audio class="col-12" src="" controls><source></audio>
-        </div>`;
-})*/
