c1192b5d172c9bcf2c6e0949ba9571d4d6adf736
XausDev <eric.xc@hotmail.com>
2024-02-20 14:19:41 +0100

    funcion selección y mejoras diseño

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const ul = document.getElementById('music-list');
 function pintarResultados(items) {
 
     ul.innerHTML = "";//Esto hace que se "recargue" el listado
+    
+    console.log(items);
 
     ordenarResultados(items);
-
-    console.log(items);
 
     items.forEach((item, index) => {
         const li = document.createElement('li');
@@ -23,7 +23,14 @@ function pintarResultados(items) {
         ul.appendChild(li);
 
         li.addEventListener('click', () => {
-            li.style.backgroundColor = '#EDEEEE';
+
+            document.querySelectorAll('li').forEach(function(li) { 
+                //Elimina la clase .seleccionado de todos los li anteriores
+                li.classList.remove('seleccionado')
+            });
+            
+            li.classList.add('seleccionado'); //Marcamos el li añadiendo la clase .seleccionado
+            
             let controls = document.getElementById('display-controls'); // Accede al elemento li que fue clickeado
             let previewUrl = item.previewUrl;
             if (item.kind == 'song') {
@@ -39,22 +46,28 @@ function pintarResultados(items) {
                 <div class="row-12 controles">
                     <audio class="col-12" src="${previewUrl}" controls><source></audio>
                 </div>`;
-            } else if (item.kind == 'tv-episode') {
+            } else if (item.kind == 'tv-episode' || item.kind == 'feature-movie' || item.kind == 'music-video') {
                 controls.innerHTML = `
                 <div id="videoContent" class="col-12">
-                    <iframe src="${previewUrl}" width="100%" height="100%" frameborder="0"></iframe>
+                    <iframe src="${previewUrl}" width="100%" height="300px" frameborder="0"></iframe>
+                </div>
+                <div class="row-12">
+                    <img src="images/camara-de-video.png" alt="Icono" width="12" height="12">
+                    <span class="col-10">${item.trackName} - ${item.artistName}</span>
                 </div>`;
             }
         });
+
     });
 }
 
-function ordenarResultados(items){ //Se asigna un atributo ordenación segun kind y luego se ordena
+
+function ordenarResultados(items){ //Se asigna un atributo "ordenación" segun el kind y luego se ordena
 
     items.forEach((item) => {
         if (item.kind === "song") {
             item.ordenacion = 1;
-        } else if (item.kind === "tv-episode") {
+        } else if (item.kind === "tv-episode" || item.kind == 'feature-movie' || item.kind == 'music-video') {
             item.ordenacion = 2;
         } else {
             item.ordenacion = 3;
@@ -75,34 +88,19 @@ function ordenarResultados(items){ //Se asigna un atributo ordenación segun kin
 }
 
 
-
-
-/*const url='https://itunes.apple.com/search?term=queen';
-
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => {console.log(data.results);})
-    .catch(error => console.log('Request failed:', error))
-    /*.then(json => console.log(json));*/
-
-
 function buscarAPI() {
+
     let searchText = document.getElementById("searchText").value;
     let busqueda = searchText.replaceAll(" ", "+");
 
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://itunes.apple.com/search?term=" + busqueda);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            let response = JSON.parse(xhr.responseText);
-            //console.log(response);
-            pintarResultados(response.results);
-        } else {
-            console.log("Error" + xhr.statusText);
-        }
-    };
-    xhr.onerror = function() {
-        console.log("Error de red");
-    };
-    xhr.send();
+    const url='https://itunes.apple.com/search?term='+ busqueda +'&country=ES&attribute=allArtistTerm';
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            //console.log(data.results);
+            pintarResultados(data.results);
+        })
+        .catch(error => console.log('Request failed:', error))
+        .then(json => console.log(json));
 };
